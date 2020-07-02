3d5c141675cecac65f56a6a8870b6965ef243647
Артур Гилязов <arik2517@outlook.com>
2020-07-02 23:57:23 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int c = scanner.nextInt();
+        String str = Integer.toString(c);
+        System.out.println(str);
+
+    }
+}
